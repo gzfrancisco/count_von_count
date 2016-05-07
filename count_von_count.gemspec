53ce175f0d9edb6347a67456d1e21212dbf69e49
bpineda <_@bernardopineda.mx>
2016-05-06 19:58:31 -0500

    Added templates and files to be included for the template

--- count_von_count.gemspec
+++ count_von_count.gemspec
@@ -1,10 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'count_von_count'
-  s.version = '0.0.6'
+  s.version = '0.1.3'
   s.date = '2016-04-07'
   s.summary = 'Simple gem'
   s.license = 'GNU GPL3'
   s.author = ['Francisco Granados', 'Bernardo Pineda']
-  s.files = ['lib/count_von_count.rb', 'lib/generators/count_von_count/install_generator.rb']
+  s.files = [ 'lib/count_von_count.rb', 
+              'lib/generators/count_von_count/install_generator.rb',
+              'lib/generators/templates/count_migration.rb']
   s.add_runtime_dependency 'rails', '> 3.2', '< 5.1'
 end
--- lib/generators/count_von_count/install_generator.rb
+++ lib/generators/count_von_count/install_generator.rb
@@ -4,10 +4,24 @@ module CountVonCount
   module Generators
     ## Generator for the Count Von Count gem
     class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+
+      source_root File.expand_path('../templates', __FILE__)
+
       def install
-        create_file 'config/initializers/initializer.rb',
-                    '# Add my file content here'
+        #create_file 'config/initializers/initializer.rb',
+        #            '# Add my file content here'
+        copy_migration "count_migration"
       end
+
+      def copy_migration(filename)
+        if self.class.migration_exists?("db/migrate", "#{filename}")
+          say_status("skipped", "Migration #{filename}.rb already exists")
+        else
+          migration_template "migrations/#{filename}.rb", "db/migrate/#{filename}.rb"
+        end
+      end
+
     end
   end
 end
